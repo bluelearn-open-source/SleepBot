391b8e3d65487c411984eef6ad04f80a8fd71773
Yog <yog.panjarale@gmail.com>
2021-08-09 18:16:42 +0530

    task loop done

--- Astronomy.py
+++ Astronomy.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands,tasks
 import requests
 import datetime
 class Astronomy(commands.Cog):
@@ -8,23 +8,44 @@ class Astronomy(commands.Cog):
 	"""
     def __init__(self, client):
         self.client = client 
-    
     @commands.command(name='pic',help="Returns the picture of the day.")
-    async def pic(self, ctx):
+    async def pic(self, ctx: commands.Context):
         url = "https://api.nasa.gov/planetary/apod?api_key=PFmJjSpr3F8mYvFd0DveMBZfQPlqjqXnpfV6LJMD"
         r= requests.get(url)
         result = r.json()
         name = requests.get('https://api.thingspeak.com/apps/thinghttp/send_request?api_key=N70VM2PR9GWHVUPM').text
         # await ctx.send(result)
         # print(result)
-        embed=discord.Embed(title=result['title'], url=result['hdurl'], description=result['explanation'], color=0x9580ff)
-        embed.set_author(name=name)
-        embed.set_thumbnail(url=result['hdurl'])
-        embed.set_footer(text=f"{result['date']} - {name} - (Nasa Pic of the day)[https://apod.nasa.gov/apod/astropix.html] ")
+        embed=discord.Embed(title=result['title'], url='https://apod.nasa.gov/apod/astropix.html', description=result['explanation'], color=0x4600b0)
+        # embed.set_thumbnail(url=result['hdurl'])
+        a=f"Astronomy Picture of The Day for {datetime.date.today().strftime('%d, %b %Y')}"
+        embed.set_author(name=a)
+        embed.set_footer(text = f"Requested by {ctx.message.author}", icon_url = ctx.message.author.avatar_url)
+        embed.add_field(name=" ⬞",value="  ⬞")
+
         embed.set_image(url=result['hdurl'])
         await ctx.send(embed=embed)
-        await ctx.send(result['hdurl'])
-
+        # await ctx.send(result['hdurl'])
+    @tasks.loop(hours=1)
+    async def send_pic(self):
+        if datetime.datetime.now().hour !=8:return
+        ASTRONOMY_CHANNEL = 564897396801984128
+        ctx= self.client.get_channel(ASTRONOMY_CHANNEL)
+        url = "https://api.nasa.gov/planetary/apod?api_key=PFmJjSpr3F8mYvFd0DveMBZfQPlqjqXnpfV6LJMD"
+        r= requests.get(url)
+        result = r.json()
+        name = requests.get('https://api.thingspeak.com/apps/thinghttp/send_request?api_key=N70VM2PR9GWHVUPM').text
+        # await ctx.send(result)
+        # print(result)
+        embed=discord.Embed(title=result['title'], url='https://apod.nasa.gov/apod/astropix.html', description=result['explanation'], color=0x4600b0)
+        # embed.set_thumbnail(url=result['hdurl'])
+        a=f"Astronomy Picture of The Day for {datetime.date.today().strftime('%d, %b %Y')}"
+        embed.set_author(name=a)
+        embed.set_footer(text = f"Requested by Time")
+        embed.set_image(url=result['hdurl'])
+        embed.add_field(name=" ⬞",value="  ⬞")
+        await ctx.send(embed=embed)
+        # await ctx.send(result['hdurl'])
 def setup(client):
 	client.add_cog(Astronomy(client))
 
